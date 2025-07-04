d4c5ddeb4d61b15d04d650731edf1593554ea8ed
soumyawis <soumya98.wis@gmail.com>
2025-07-04 13:45:31 +0530

    fix middleware error

--- frontent/next.config.ts
+++ frontent/next.config.ts
@@ -1,10 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export", // for static export
-  experimental: {
-  },
+  
 };
 
 export default nextConfig;
