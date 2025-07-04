a63a81feedcd3d266da05e967149adce72a1f5b9
soumyawis <soumya98.wis@gmail.com>
2025-07-04 16:07:01 +0530

    fix cookie error

--- backend/src/utils/jwtToken.ts
+++ backend/src/utils/jwtToken.ts
@@ -7,8 +7,9 @@ export const genarateToken = (userId: string, res: Response) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production", // Use secure cookies in production
-    sameSite: "strict", // Helps prevent CSRF attacks
+    secure: process.env.NODE_ENV === "production", // true in production
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    domain: "https://macauthenticate.onrender.com", // set this if frontend and backend are on the same root domain
   });
   return token;
 };
